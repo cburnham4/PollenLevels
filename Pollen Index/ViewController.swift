f6b155d4aeeb50ab74a2f963f8b986a77eda0fbc
Chase Burnham <cburnhamiv@gmail.com>
2019-05-05 15:58:56 -0400

    Initial Commit

---
+++ Pollen Index/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pollen Index
+//
+//  Created by Carl Burnham on 5/5/19.
+//  Copyright © 2019 Carl Burnham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
